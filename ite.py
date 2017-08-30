646c07c2283d6999d0ae8796411c713384d775d8
Jacob Mischka <jacob@mischka.me>
2017-08-29 21:25:50 -0500

    Start xlsx dumper, r1c1 doesn't work darn

--- ite.py
+++ ite.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+from openpyxl import Workbook
 import csv, sys
+
+# FIXME
+ORIGINAL_DATA_COL_RANGE = ('C', 'J')
+DIFF_COL_RANGE = ('L', 'O')
+MISSED_COL_RANGE = ('Q', 'T')
 
 def extract(inpath):
 	rows = []
@@ -99,90 +105,114 @@ def parse_percentage(text):
 def format_percentage(num):
 	return '{}%'.format(num * 100)
 
+def get_xlsx_rows(sections):
+	# TODO: Averages
+	rows = []
+
+	for section in sections:
+		rows += section.get_xlsx_rows()
+		rows.append([])
+
+	rows.append([])
+
+	return rows
+
+def get_csv_rows(sections):
+	rows = []
+	items = []
+
+	for section in sections:
+		rows += section.get_csv_rows()
+		rows.append([])
+		items += section.items
+
+	rows.append([])
+
+	rows.append([
+		'Overall averages',
+		'',
+		sum([item.cby_total for item in items])/len(items),
+		sum([item.cby for item in items])/len(items),
+		sum([item.ca1_total for item in items])/len(items),
+		sum([item.ca1 for item in items])/len(items),
+		sum([item.ca2_total for item in items])/len(items),
+		sum([item.ca2 for item in items])/len(items),
+		sum([item.ca3_total for item in items])/len(items),
+		sum([item.ca3 for item in items])/len(items),
+		'',
+		sum([item.cby_diff for item in items])/len(items),
+		sum([item.ca1_diff for item in items])/len(items),
+		sum([item.ca2_diff for item in items])/len(items),
+		sum([item.ca3_diff for item in items])/len(items),
+		'',
+		sum([item.cby_missed for item in items])/len(items),
+		sum([item.ca1_missed for item in items])/len(items),
+		sum([item.ca2_missed for item in items])/len(items),
+		sum([item.ca3_missed for item in items])/len(items)
+	])
+
+	advanced_items = [item for item in items if item.item_type == 'A']
+	basic_items = [item for item in items if item.item_type == 'B']
+
+	rows.append([
+		'Advanced averages',
+		'',
+		sum([item.cby_total for item in advanced_items])/len(advanced_items),
+		sum([item.cby for item in advanced_items])/len(advanced_items),
+		sum([item.ca1_total for item in advanced_items])/len(advanced_items),
+		sum([item.ca1 for item in advanced_items])/len(advanced_items),
+		sum([item.ca2_total for item in advanced_items])/len(advanced_items),
+		sum([item.ca2 for item in advanced_items])/len(advanced_items),
+		sum([item.ca3_total for item in advanced_items])/len(advanced_items),
+		sum([item.ca3 for item in advanced_items])/len(advanced_items),
+		'',
+		sum([item.cby_diff for item in advanced_items])/len(advanced_items),
+		sum([item.ca1_diff for item in advanced_items])/len(advanced_items),
+		sum([item.ca2_diff for item in advanced_items])/len(advanced_items),
+		sum([item.ca3_diff for item in advanced_items])/len(advanced_items),
+		'',
+		sum([item.cby_missed for item in advanced_items])/len(advanced_items),
+		sum([item.ca1_missed for item in advanced_items])/len(advanced_items),
+		sum([item.ca2_missed for item in advanced_items])/len(advanced_items),
+		sum([item.ca3_missed for item in advanced_items])/len(advanced_items)
+	])
+	rows.append([
+		'Basic averages',
+		'',
+		sum([item.cby_total for item in basic_items])/len(basic_items),
+		sum([item.cby for item in basic_items])/len(basic_items),
+		sum([item.ca1_total for item in basic_items])/len(basic_items),
+		sum([item.ca1 for item in basic_items])/len(basic_items),
+		sum([item.ca2_total for item in basic_items])/len(basic_items),
+		sum([item.ca2 for item in basic_items])/len(basic_items),
+		sum([item.ca3_total for item in basic_items])/len(basic_items),
+		sum([item.ca3 for item in basic_items])/len(basic_items),
+		'',
+		sum([item.cby_diff for item in basic_items])/len(basic_items),
+		sum([item.ca1_diff for item in basic_items])/len(basic_items),
+		sum([item.ca2_diff for item in basic_items])/len(basic_items),
+		sum([item.ca3_diff for item in basic_items])/len(basic_items),
+		'',
+		sum([item.cby_missed for item in basic_items])/len(basic_items),
+		sum([item.ca1_missed for item in basic_items])/len(basic_items),
+		sum([item.ca2_missed for item in basic_items])/len(basic_items),
+		sum([item.ca3_missed for item in basic_items])/len(basic_items)
+	])
+
+	return rows
 
 def dump_section_csv(sections, outpath):
 	with open(outpath, 'w') as outfile:
 		writer = csv.writer(outfile)
+		writer.writerows(get_csv_rows(sections))
 
-		items = []
+def dump_section_xlsx(sections, outpath):
+	wb = Workbook(write_only=True)
+	ws = wb.create_sheet()
+	for row in get_xlsx_rows(sections):
+		ws.append(row)
 
-		for section in sections:
-			writer.writerows(section.get_rows())
-			writer.writerow([])
-			items += section.items
-
-		writer.writerow([])
-
-		writer.writerow([
-			'Overall averages',
-			'',
-			sum([item.cby_total for item in items])/len(items),
-			sum([item.cby for item in items])/len(items),
-			sum([item.ca1_total for item in items])/len(items),
-			sum([item.ca1 for item in items])/len(items),
-			sum([item.ca2_total for item in items])/len(items),
-			sum([item.ca2 for item in items])/len(items),
-			sum([item.ca3_total for item in items])/len(items),
-			sum([item.ca3 for item in items])/len(items),
-			'',
-			sum([item.cby_diff for item in items])/len(items),
-			sum([item.ca1_diff for item in items])/len(items),
-			sum([item.ca2_diff for item in items])/len(items),
-			sum([item.ca3_diff for item in items])/len(items),
-			'',
-			sum([item.cby_missed for item in items])/len(items),
-			sum([item.ca1_missed for item in items])/len(items),
-			sum([item.ca2_missed for item in items])/len(items),
-			sum([item.ca3_missed for item in items])/len(items)
-		])
-
-		advanced_items = [item for item in items if item.item_type == 'A']
-		basic_items = [item for item in items if item.item_type == 'B']
-
-		writer.writerow([
-			'Advanced averages',
-			'',
-			sum([item.cby_total for item in advanced_items])/len(advanced_items),
-			sum([item.cby for item in advanced_items])/len(advanced_items),
-			sum([item.ca1_total for item in advanced_items])/len(advanced_items),
-			sum([item.ca1 for item in advanced_items])/len(advanced_items),
-			sum([item.ca2_total for item in advanced_items])/len(advanced_items),
-			sum([item.ca2 for item in advanced_items])/len(advanced_items),
-			sum([item.ca3_total for item in advanced_items])/len(advanced_items),
-			sum([item.ca3 for item in advanced_items])/len(advanced_items),
-			'',
-			sum([item.cby_diff for item in advanced_items])/len(advanced_items),
-			sum([item.ca1_diff for item in advanced_items])/len(advanced_items),
-			sum([item.ca2_diff for item in advanced_items])/len(advanced_items),
-			sum([item.ca3_diff for item in advanced_items])/len(advanced_items),
-			'',
-			sum([item.cby_missed for item in advanced_items])/len(advanced_items),
-			sum([item.ca1_missed for item in advanced_items])/len(advanced_items),
-			sum([item.ca2_missed for item in advanced_items])/len(advanced_items),
-			sum([item.ca3_missed for item in advanced_items])/len(advanced_items)
-		])
-		writer.writerow([
-			'Basic averages',
-			'',
-			sum([item.cby_total for item in basic_items])/len(basic_items),
-			sum([item.cby for item in basic_items])/len(basic_items),
-			sum([item.ca1_total for item in basic_items])/len(basic_items),
-			sum([item.ca1 for item in basic_items])/len(basic_items),
-			sum([item.ca2_total for item in basic_items])/len(basic_items),
-			sum([item.ca2 for item in basic_items])/len(basic_items),
-			sum([item.ca3_total for item in basic_items])/len(basic_items),
-			sum([item.ca3 for item in basic_items])/len(basic_items),
-			'',
-			sum([item.cby_diff for item in basic_items])/len(basic_items),
-			sum([item.ca1_diff for item in basic_items])/len(basic_items),
-			sum([item.ca2_diff for item in basic_items])/len(basic_items),
-			sum([item.ca3_diff for item in basic_items])/len(basic_items),
-			'',
-			sum([item.cby_missed for item in basic_items])/len(basic_items),
-			sum([item.ca1_missed for item in basic_items])/len(basic_items),
-			sum([item.ca2_missed for item in basic_items])/len(basic_items),
-			sum([item.ca3_missed for item in basic_items])/len(basic_items)
-		])
+	wb.save(outpath)
 
 def main():
 	labels, body = extract('/home/mischka/Downloads/ite and basic stuff/ITE_ProgramItem_156002.txt')
@@ -190,9 +220,31 @@ def main():
 	# dump_csv(labels, body, './output/2017-ite.csv')
 	sections = extract_sections(body)
 	dump_section_csv(sections, './output/2017-ite-sections.csv')
+	dump_section_xlsx(sections, './output/2017-ite-sections.xlsx')
 
 
 class IteSection(object):
+	HEADINGS = [
+		'',
+		'% total CBY',
+		'% our CBY',
+		'% total CA-1',
+		'% our CA-1',
+		'% total CA-2',
+		'% our CA-2',
+		'% total CA-3',
+		'% our CA-3',
+		'',
+		'CBY',
+		'CA-1',
+		'CA-2',
+		'CA-3',
+		'',
+		'CBY',
+		'CA-1',
+		'CA-2',
+		'CA-3'
+	]
 
 	def __init__(self, heading, subheading, items):
 		self.heading = heading
@@ -200,31 +252,22 @@ class IteSection(object):
 		self.items = [IteItem(item) for item in items]
 
 
-	def get_rows(self):
+	def get_csv_rows(self):
 		return [
 			[
 				self.heading,
-				'',
-				'% total CBY',
-				'% our CBY',
-				'% total CA-1',
-				'% our CA-1',
-				'% total CA-2',
-				'% our CA-2',
-				'% total CA-3',
-				'% our CA-3',
-				'',
-				'CBY',
-				'CA-1',
-				'CA-2',
-				'CA-3',
-				'',
-				'CBY',
-				'CA-1',
-				'CA-2',
-				'CA-3'
+				*self.HEADINGS
 			],
-			*[item.get_row() for item in self.items]
+			*[item.get_csv_row() for item in self.items]
+		]
+
+	def get_xlsx_rows(self):
+		return [
+			[
+				self.heading,
+				*self.HEADINGS
+			],
+			*[item.get_xlsx_row() for item in self.items]
 		]
 
 	def __repr__(self):
@@ -283,18 +326,18 @@ class IteItem(object):
 	def ca3_missed(self):
 		return 1 - self.ca3
 
-	def get_row(self):
+	def get_csv_row(self):
 		return [
 			self.keyword,
 			'',
-			self.ca3_total,
-			self.ca3,
-			self.ca2_total,
-			self.ca2,
-			self.ca1_total,
-			self.ca1,
 			self.cby_total,
 			self.cby,
+			self.ca1_total,
+			self.ca1,
+			self.ca2_total,
+			self.ca2,
+			self.ca3_total,
+			self.ca3,
 			'',
 			self.cby_diff,
 			self.ca1_diff,
@@ -305,6 +348,30 @@ class IteItem(object):
 			self.ca1_missed,
 			self.ca2_missed,
 			self.ca3_missed
+		]
+
+	def get_xlsx_row(self):
+		return [
+			self.keyword,
+			'',
+			self.cby_total,
+			self.cby,
+			self.ca1_total,
+			self.ca1,
+			self.ca2_total,
+			self.ca2,
+			self.ca3_total,
+			self.ca3,
+			'',
+			'=(RC[-8] - RC[-9])',
+			'=(RC[-7] - RC[-8])',
+			'=(RC[-6] - RC[-7])',
+			'=(RC[-5] - RC[-6])',
+			'',
+			'=(1 - R[0]C[-13])',
+			'=(1 - R[0]C[-12])',
+			'=(1 - R[0]C[-11])',
+			'=(1 - R[0]C[-10])'
 		]
 
 	def __repr__(self):
